360af88c4be1d3669039f9a2bb995fbec866b2a1
Defu Lian <dlian@hydrus13.eng.uts.edu.au>
2017-04-07 11:37:15 +1000

    re-organize the structure of libary

--- item_recommend.m
+++ item_recommend.m
@@ -1,5 +1,6 @@
 function metric = item_recommend(rec, mat, varargin)
 option = 'output';
+
 for i=1:2:length(varargin)
     if strcmp(varargin{i}, 'test') || strcmp(varargin{i}, 'split_ratio')
         option = 'heldout';
@@ -10,12 +11,12 @@ for i=1:2:length(varargin)
     end
 end
 if strcmp(option, 'heldout')
-    metric = heldout_rec(rec, mat, @compute_score_item, varargin{:});
+    metric = heldout_rec(rec, mat, @evaluate_item, varargin{:});
 end
 if strcmp(option, 'crossvalid')
-    metric = crossvalid_rec(rec, mat, @compute_score_item, varargin{:});
+    metric = crossvalid_rec(rec, mat, @evaluate_item, varargin{:});
 end
 if strcmp(option, 'output')
-    metric = heldout_rec(rec, mat, @compute_score_item, 'test', sparse(size(mat,1), size(mat,2)), varargin{:});
+    metric = heldout_rec(rec, mat, @evaluate_item, 'test', sparse(size(mat,1), size(mat,2)), varargin{:});
 end
 end
--- script.m
+++ script.m
@@ -1,7 +1,9 @@
-mex -largeArrayDims iccf_sub.cpp
-[train, test] = readData('D:\BaiduNetdiskDownload\checkin_rerun_tkde', 1);
-metric1 = item_recommend(@iccf, R, 'test', test, 'topk', 200, 'max_iter', 10);
-metric = item_recommend(@iccf, R, 'test', test, 'max_iter', 10);
-metric = item_recommend(@iccf, R, 'split_ratio', 0.8, 'topk', 200, 'max_iter', 10, 'split_mode', 'en');
+
+[train, test] = readData('/home/dlian/data/subcheckin', 1);
+
+data = train+test;
+R = data>0;
+metric1 = item_recommend(@iccf, train>0, 'test', test, 'topk', 200, 'max_iter', 10);
+metric2 = item_recommend(@iccf, R, 'folds', 5, 'topk', 200, 'fold_mode', 'u');
 %output_wobias0 = iccf(R, 'K',50, 'max_iter', max_iter);
 
