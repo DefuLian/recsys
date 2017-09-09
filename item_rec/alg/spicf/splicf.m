ec3fd96bb0708f158048a2177c55d3cfd6abc773
Defu Lian <dlian@titan2.eng.uts.edu.au>
2017-09-09 12:40:20 +1000

    fixing bugs in self paced learning

--- item_rec/alg/spicf/splicf.m
+++ item_rec/alg/spicf/splicf.m
@@ -1,29 +1,25 @@
 function [P, Q] = splicf(R, varargin)
-    [beta, test, max_iter, K, reg_u, reg_i, spr] = process_options(varargin, 'beta', 0.1, 'test', [], 'max_iter', 20, 'K', 50, ...
+    [beta, test, max_iter, K, reg_u, reg_i, spr] = process_options(varargin, 'beta', 1/30, 'test', [], 'max_iter', 5, 'K', 50, ...
         'reg_u', 0.01, 'reg_i', 0.01, 'spr', 'lin_soft');
-    [~, N] = size(R); eta = 0.5;
+    [~, N] = size(R); eta = 0.8;
     Rt = R.';
     Q = randn(N, K) * 0.1;
     W = +(R>0);
     inner_max_iter = 5;
-    for inner_iter=1:inner_max_iter
-        P = optimize_newton(Rt, Q, W.', beta, reg_u);
-        Q = optimize_newton(R, P, W, beta, reg_i);
-        fprintf('    inner #iteration=%d, loss=%f\n', inner_iter, compute_loss(R, P, Q, beta));
-    end
-    
-    [k_small, k_large] = initial_pace(R, P, Q, eta);
-    
+
     for iter=1:max_iter
-        eval = evaluate_item(R, test, P, Q, 200, 200);
-        fprintf('outer iteration #=%3d of SPL, ndcg@50=%f, ', iter, eval.ndcg(50))
-        W = optimize_weight(R, P, Q, k_small, k_large, spr);
-        fprintf('#pos_example=%d/%d, #neg_example=%d/%d\n', full(sum(sum((W>0) & (R>0)))), full(sum(R(:)>0)), full(sum(sum((W>0) & (R<0)))), full(sum(R(:)<0)));
-        for i=1:inner_max_iter
+        for inner_iter=1:inner_max_iter
             P = optimize_newton(Rt, Q, W.', beta, reg_u);
             Q = optimize_newton(R, P, W, beta, reg_i);
             fprintf('    inner #iteration=%d, loss=%f\n', inner_iter, compute_loss(R, P, Q, beta));
         end
+        [k_small, k_large] = compute_pace(R, P, Q, eta, 0.5 + 0.5 * (iter-1)/ max_iter);
+        eval = evaluate_item(R, test, P, Q, -1, 200);
+        fprintf('outer iteration #=%3d of SPL, recall@50=%f, ndcg@50=%f, auc=%f\n', iter, eval.recall(50), eval.ndcg(50), eval.auc());
+        W = optimize_weight(R, P, Q, k_small, k_large, spr);
+        fprintf('k_small=[%f,%f], k_large=[%f,%f]\n', ...
+            k_small(1), k_small(2), k_large(1),k_large(2));
+        fprintf('%s\n', stat(W,R));
     end
 end
 
@@ -42,7 +38,7 @@ function P = optimize_newton(Rt, Q, Wt, beta, reg)
         X = Q(ind, :);
         y = (col_r(ind) + 1) / 2; % convert {+1,-1} to {1, 0}
         XtX = 2 * beta * (QtQ - X.' * X) + 2 * reg * eye(K);
-        while(true)
+        for iter = 1:50
             mu = 1./(1 + exp(-X * p));
             s = w .* mu .* (1 - mu);
             XsX = X.' * spdiags(s, 0, length(s), length(s)) * X;
@@ -88,15 +84,15 @@ function W = optimize_weight(R, P, Q, k_small_array, k_large_array, choice)
         if strcmp(choice, 'lin_soft')
             % w = b - a x
             a = k_large * k_small / (k_large - k_small); b = k_large / (k_large - k_small);
-            w(small_index & ~tiny_index) = b - a * w(small_index & ~tiny_index);
+            w(small_index & ~tiny_index) = b - a * loss(small_index & ~tiny_index);
         elseif strcmp(choice, 'mixture_soft')
             % w = a/x - b
             a = 1/(k_large-k_small); b = k_small / (k_large - k_small);
-            w(small_index & ~tiny_index) = a ./ w(small_index & ~tiny_index) - b;
+            w(small_index & ~tiny_index) = a ./ loss(small_index & ~tiny_index) - b;
         elseif strcmp(choice, 'sqrt_soft')
             % w = a/sqrt(l) - b
-            a = 1/(sqrt(k_large) - sqrt(k_small)); b = sqrt(k_small);
-            w(small_index & ~tiny_index) = a ./ sqrt(w(small_index & ~tiny_index)) - b;
+            a = 1/(sqrt(k_large) - sqrt(k_small)); b = sqrt(k_small)/(sqrt(k_large) - sqrt(k_small));
+            w(small_index & ~tiny_index) = a ./ sqrt(loss(small_index & ~tiny_index)) - b;
         end
         I_list{i} = I;
         J_list{i} = J;
@@ -104,19 +100,20 @@ function W = optimize_weight(R, P, Q, k_small_array, k_large_array, choice)
     end
     W = sparse(cell2mat(I_list), cell2mat(J_list), cell2mat(w_list), M, N);
 end
-function [k_small, k_large] = initial_pace(R, P, Q, eta)
-    k_small = zeros(2,1);
+function [k_small, k_large] = compute_pace(R, P, Q, eta, q)
+    k_small = zeros(2,1); k_large = zeros(2,1);
     [I, J, y] = find(R == -1);
     y_hat = sum(P(I,:) .* Q(J, :), 2);
     loss = log(1+exp(-y .* y_hat));
-    k_small(1) = 1/median(loss);
+    k_small(1) = 1/quantile(loss, q);
+    k_large(1) = 1/quantile(loss, q*eta);
     
     [I, J, y] = find(R == 1);
     y_hat = sum(P(I,:) .* Q(J, :), 2);
     loss = log(1+exp(-y .* y_hat));
-    k_small(2) = max(loss);
-    
-    k_large = (1+eta)/eta * k_small;
+    k_small(2) = 1/(max(loss)+eps);
+    k_large(2) = k_small(2);
+    %k_large = (1+eta)/eta * k_small;
 end
 function total_loss = compute_loss(R, P, Q, beta)
     [I, J, y] = find(R);
@@ -125,4 +122,26 @@ function total_loss = compute_loss(R, P, Q, beta)
     total_loss = sum(loss);
     total_loss = total_loss + beta*(trace((P.' * P) .* (Q.' * Q)) - sum(y_hat.^2));
 end
+function str = stat(W, R)
+    [M, N] = size(R);
+    pos_ind = R>0;
+    neg_ind = R<0;
+    [I, J, ~] = find(pos_ind);
+    pos_w = full(W(sub2ind([M, N], I, J))); 
+    [I, J, ~] = find(neg_ind);
+    neg_w = full(W(sub2ind([M, N], I, J)));
+    
+    pos_ratio = sum(pos_w>0) / length(pos_w);
+    neg_ratio = sum(neg_w>0) / length(neg_w);
+    
+    pos_qr = quantile(pos_w, [0, 0.25, 0.5, 0.75, 1]);
+    neg_qr = quantile(neg_w(neg_w>0), [0, 0.25, 0.5, 0.75, 1]);
+    
+    pos_1_ratio = sum(pos_w > 0.99) / length(pos_w);
+    neg_1_ratio = sum(neg_w > 0.99) / length(neg_w);
+    
+    str1 = sprintf('pos_ratio=%f, neg_ratio=%f\npos_1_ratio=%f, neg_1_ratio=%f', pos_ratio, neg_ratio, pos_1_ratio, neg_1_ratio);
+    str = sprintf('%s\n[%s],[%s]', str1, sprintf('%.3f,', pos_qr), sprintf('%.3f,', neg_qr));
+end
+
 
--- item_rec/compute_item_metric.m
+++ item_rec/compute_item_metric.m
@@ -37,7 +37,7 @@ end
 function AUC = ComputeAUC(user_rank, cand_count, rel_count)
 auc_vector = rel_count .* cand_count - sum(user_rank, 2) - (rel_count .* (rel_count - 1))/2;
 auc_vector = auc_vector ./ ((cand_count - rel_count) .* rel_count);
-AUC = mean(auc_vector);
+AUC = full(mean(auc_vector));
 end
 
 function [ prec, recall, map ] = PrecRecall(user_rank, user_count, k )
